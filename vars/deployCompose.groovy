014f6f0582c6601ae08cf6d57f5b8d759d2ace26
Cesar Nunez <ca.nunezhuaman@gmail.com>
2026-03-11 11:12:05 -0500

    editions to deploy stage

--- vars/deployCompose.groovy
+++ vars/deployCompose.groovy
@@ -3,6 +3,7 @@ def call(Map cfg = [:]) {
         checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: cfg.deployRepo]]])
         
         withEnv(["IMAGE_URI=${cfg.imageUri}", "IMAGE_TAG=${cfg.imageTag}", "ENV=${cfg.pipelineEnv}"]) {
+            chmod +x "./scripts/deploy-${cfg.pipelineEnv}.sh"
             sh "./scripts/deploy-${cfg.pipelineEnv}.sh"
         }
     }
