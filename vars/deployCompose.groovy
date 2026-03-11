76aa3fa59dd55ae9a74c6bf94df0fa15ae52dac0
Cesar Nunez <ca.nunezhuaman@gmail.com>
2026-03-11 11:15:35 -0500

    editions to deploy stage

--- vars/deployCompose.groovy
+++ vars/deployCompose.groovy
@@ -1,10 +1,25 @@
 def call(Map cfg = [:]) {
     dir('central-deploy-repo') {
-        checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: cfg.deployRepo]]])
-        
-        withEnv(["IMAGE_URI=${cfg.imageUri}", "IMAGE_TAG=${cfg.imageTag}", "ENV=${cfg.pipelineEnv}"]) {
-            chmod +x "./scripts/deploy-${cfg.pipelineEnv}.sh"
-            sh "./scripts/deploy-${cfg.pipelineEnv}.sh"
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/main']],
+            userRemoteConfigs: [[url: cfg.deployRepo]]
+        ])
+
+        def scriptPath = "./scripts/deploy-${cfg.pipelineEnv}.sh"
+
+        withEnv([
+            "IMAGE_URI=${cfg.imageUri}",
+            "IMAGE_TAG=${cfg.imageTag}",
+            "ENV=${cfg.pipelineEnv}"
+        ]) {
+            sh """
+                set -e
+                test -f ${scriptPath}
+                sed -i 's/\\r\$//' ${scriptPath} || true
+                chmod +x ${scriptPath}
+                bash ${scriptPath}
+            """
         }
     }
 }
